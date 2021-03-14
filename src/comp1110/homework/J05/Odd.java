eed4c70ed8ab9d5ca946291392e6ceb324570253
Ke Ning <u7175553@anu.edu.au>
2021-03-14 22:58:06 +0800

    j05 Odd done!

---
+++ src/comp1110/homework/J05/Odd.java
@@ -0,0 +1,22 @@
+package comp1110.homework.J05;
+
+import java.util.Scanner;
+
+public class Odd {
+
+    public static boolean isOdd(int i){
+        return i % 2 != 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        if (isOdd(number)){
+            System.out.println(number + " is odd");
+        }else {
+            System.out.println(number + " is even");
+        }
+
+    }
+}
