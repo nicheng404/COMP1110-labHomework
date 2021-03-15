7d18799a6d35e994a96e8ef969ffd0073a3a6265
Ke Ning <u7175553@anu.edu.au>
2021-03-15 13:53:57 +0800

    O01 BMI done!

---
+++ src/comp1110/homework/O01/BMI.java
@@ -0,0 +1,27 @@
+package comp1110.homework.O01;
+
+public class BMI {
+    String name;
+    double weight;
+    double height;
+
+    public BMI( String name, double height,double weight){
+        this.name=name;
+        this.height=height;
+        this.weight=weight;
+    }
+
+    public double getBMI(){
+        return weight/(height*height);
+    }
+
+    public String toString(){
+
+        return name+" is "+height+"m tall and is "+weight+"Kg and has a BMI of "+weight/(height*height)+"Kg/m^2";
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
